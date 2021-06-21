7f341b00d850577cf866caecbee61a102144156e
Manoel Marques <Manoel.Marques@ibm.com>
2021-06-21 16:19:12 -0400

    Improvements to deprecate methods (#172)

--- qiskit_optimization/deprecation.py
+++ qiskit_optimization/deprecation.py
@@ -12,20 +12,68 @@
 
 """Contains the Deprecation message methods."""
 
+from abc import abstractmethod
 import warnings
 import functools
 from typing import NamedTuple, Optional, Callable, Dict, Set
-from enum import Enum
+from enum import Enum, EnumMeta
+
+
+class DeprecatedEnum(Enum):
+    """
+    Shows deprecate message whenever member is accessed
+    """
+
+    def __new__(cls, value, *args):
+        member = object.__new__(cls)
+        member._value_ = value
+        member._args = args
+        member._show_deprecate = member.deprecate
+        return member
+
+    @abstractmethod
+    def deprecate(self):
+        """show deprecate message"""
+        pass
+
+
+class DeprecatedEnumMeta(EnumMeta):
+    """
+    Shows deprecate message whenever member is accessed
+    """
+
+    def __getattribute__(cls, name):
+        obj = super().__getattribute__(name)
+        if isinstance(obj, DeprecatedEnum) and obj._show_deprecate:
+            obj._show_deprecate()
+        return obj
+
+    def __getitem__(cls, name):
+        member = super().__getitem__(name)
+        if member._show_deprecate:
+            member._show_deprecate()
+        return member
+
+    # pylint: disable=redefined-builtin
+    def __call__(cls, value, names=None, *, module=None, qualname=None, type=None, start=1):
+        obj = super().__call__(
+            value, names, module=module, qualname=qualname, type=type, start=start
+        )
+        if isinstance(obj, DeprecatedEnum) and obj._show_deprecate:
+            obj._show_deprecate()
+        return obj
 
 
 class DeprecatedType(Enum):
     """ " Deprecation Types"""
 
     PACKAGE = "package"
+    ENUM = "enum"
     CLASS = "class"
     METHOD = "method"
     FUNCTION = "function"
     ARGUMENT = "argument"
+    PROPERTY = "property"
 
 
 class _DeprecatedObject(NamedTuple):
@@ -52,10 +100,10 @@ def warn_deprecated(
     """Emits deprecation warning the first time only
     Args:
         version: Version to be used
-        old_type: Old type to be used.
-        old_name: Old name of function to be used
-        new_type: New type to be used.
-        new_name: New name of function to be used
+        old_type: Old type to be used
+        old_name: Old name to be used
+        new_type: New type to be used, if None, old_type is used instead.
+        new_name: New name to be used
         additional_msg: any additional message
         stack_level: stack level
     """
@@ -70,13 +118,35 @@ def warn_deprecated(
         f"The {old_name} {old_type.value} is deprecated as of version {version} "
         "and will be removed no sooner than 3 months after the release"
     )
-    if new_type is not None and new_name:
-        msg += f". Instead use the {new_name} {new_type.value}"
+    if new_name:
+        type_str = new_type.value if new_type is not None else old_type.value
+        msg += f". Instead use the {new_name} {type_str}"
     if additional_msg:
         msg += f" {additional_msg}"
     msg += "."
 
-    warnings.warn(msg, DeprecationWarning, stacklevel=stack_level)
+    warnings.warn(msg, DeprecationWarning, stacklevel=stack_level + 1)
+
+
+def warn_deprecated_same_type_name(
+    version: str,
+    new_type: DeprecatedType,
+    new_name: str,
+    additional_msg: Optional[str] = None,
+    stack_level: int = 2,
+) -> None:
+    """Emits deprecation warning the first time only
+       Used when the type and name remained the same.
+    Args:
+        version: Version to be used
+        new_type: new type to be used
+        new_name: new name to be used
+        additional_msg: any additional message
+        stack_level: stack level
+    """
+    warn_deprecated(
+        version, new_type, new_name, new_type, new_name, additional_msg, stack_level + 1
+    )
 
 
 def _rename_kwargs(version, func_name, kwargs, kwarg_map, additional_msg, stack_level):
@@ -109,12 +179,12 @@ def deprecate_arguments(
     """Decorator to alias deprecated argument names and warn upon use.
     Args:
         version: Version to be used
-        kwarg_map: Args dictionary with old, new arguments.
+        kwarg_map: Args dictionary with old, new arguments
         additional_msg: any additional message
         stack_level: stack level
 
     Returns:
-        The decorated function.
+        The decorated function
     """
 
     def decorator(func):
@@ -131,89 +201,120 @@ def deprecate_arguments(
     return decorator
 
 
-def deprecate_method(
+def _deprecate_object(
     version: str,
+    old_type: DeprecatedType,
     new_type: DeprecatedType,
     new_name: str,
+    additional_msg: str,
+    stack_level: int,
+) -> Callable:
+    """Decorator that prints deprecated message
+    Args:
+        version: Version to be used
+        old_type: New type to be used
+        new_type: New type to be used, if None, old_type is used instead.
+        new_name: New name to be used
+        additional_msg: any additional message
+        stack_level: stack level
+
+    Returns:
+        The decorated method
+    """
+
+    def decorator(func):
+        msg = (
+            f"The {func.__name__} {old_type.value} is deprecated as of version {version} "
+            "and will be removed no sooner than 3 months after the release"
+        )
+        if new_name:
+            type_str = new_type.value if new_type is not None else old_type.value
+            msg += f". Instead use the {new_name} {type_str}"
+        if additional_msg:
+            msg += f" {additional_msg}"
+        msg += "."
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # warn only once
+            if not wrapper._warned:
+                warnings.warn(msg, DeprecationWarning, stacklevel=stack_level)
+                wrapper._warned = True
+            return func(*args, **kwargs)
+
+        wrapper._warned = False
+        return wrapper
+
+    return decorator
+
+
+def deprecate_method(
+    version: str,
+    new_type: Optional[DeprecatedType] = None,
+    new_name: Optional[str] = None,
     additional_msg: Optional[str] = None,
     stack_level: int = 2,
 ) -> Callable:
     """Decorator that prints deprecated message for an instance method
     Args:
         version: Version to be used
-        new_type: New type to be used.
-        new_name: New name of function to be used
+        new_type: New type to be used
+        new_name: New name to be used
         additional_msg: any additional message
         stack_level: stack level
 
     Returns:
-        The decorated method.
+        The decorated method
     """
+    return _deprecate_object(
+        version, DeprecatedType.METHOD, new_type, new_name, additional_msg, stack_level
+    )
 
-    def decorator(method):
-        msg = (
-            f"The {method.__name__} {DeprecatedType.METHOD.value} is deprecated "
-            f"as of version {version} and will be removed no sooner "
-            "than 3 months after the release. Instead use the "
-            f"{new_name} {new_type.value}"
-        )
-        if additional_msg:
-            msg += f" {additional_msg}"
-        msg += "."
 
-        @functools.wraps(method)
-        def wrapper(self, *method_args, **method_kwargs):
-            # warn only once
-            if not wrapper._warned:
-                warnings.warn(msg, DeprecationWarning, stacklevel=stack_level)
-                wrapper._warned = True
-            return method(self, *method_args, **method_kwargs)
+def deprecate_property(
+    version: str,
+    new_type: Optional[DeprecatedType] = None,
+    new_name: Optional[str] = None,
+    additional_msg: Optional[str] = None,
+    stack_level: int = 2,
+) -> Callable:
+    """Decorator that prints deprecated message for a property
 
-        wrapper._warned = False
-        return wrapper
+    *** This decorator must be placed below the property decorator ***
 
-    return decorator
+    Args:
+        version: Version to be used
+        new_type: New type to be used
+        new_name: New name to be used
+        additional_msg: any additional message
+        stack_level: stack level
+
+    Returns:
+        The decorated property
+    """
+    return _deprecate_object(
+        version, DeprecatedType.PROPERTY, new_type, new_name, additional_msg, stack_level
+    )
 
 
 def deprecate_function(
     version: str,
-    new_type: DeprecatedType,
-    new_name: str,
+    new_type: Optional[DeprecatedType] = None,
+    new_name: Optional[str] = None,
     additional_msg: Optional[str] = None,
     stack_level: int = 2,
 ) -> Callable:
     """Decorator that prints deprecated message for a function
     Args:
         version: Version to be used
-        new_type: New type to be used.
-        new_name: New name of function to be used
+        new_type: New type to be used
+        new_name: New name to be used
         additional_msg: any additional message
         stack_level: stack level
 
     Returns:
-        The decorated function.
+        The decorated function
     """
-
-    def decorator(func):
-        msg = (
-            f"The {func.__name__} {DeprecatedType.FUNCTION.value} is deprecated "
-            f"as of version {version} and will be removed no sooner "
-            "than 3 months after the release. Instead use the "
-            f"{new_name} {new_type.value}"
-        )
-        if additional_msg:
-            msg += f" {additional_msg}"
-        msg += "."
-
-        @functools.wraps(func)
-        def wrapper(*method_args, **method_kwargs):
-            # warn only once
-            if not wrapper._warned:
-                warnings.warn(msg, DeprecationWarning, stacklevel=stack_level)
-                wrapper._warned = True
-            return func(*method_args, **method_kwargs)
-
-        wrapper._warned = False
-        return wrapper
-
-    return decorator
+    return _deprecate_object(
+        version, DeprecatedType.FUNCTION, new_type, new_name, additional_msg, stack_level
+    )
