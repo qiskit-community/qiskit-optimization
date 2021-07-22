b0cebfe1a26526461ff29fc2862a86af6adb178a
lca <lca@zurich.ibm.com>
2021-07-22 15:59:37 +0200

    Added Sherrington Kirkpatrick (SK) model to applications

---
+++ qiskit_optimization/applications/SK_model.py
@@ -0,0 +1,104 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+from typing import Optional, Union, List
+
+import networkx as nx
+import numpy as np
+from docplex.mp.model import Model
+
+from .graph_optimization_application import OptimizationApplication
+from ..algorithms import OptimizationResult
+from ..problems.quadratic_program import QuadraticProgram
+from ..translators import from_docplex_mp
+
+
+class SKModel(OptimizationApplication):
+    """Optimization application for the "Sherrington Kirkpatrick (SK) model" [1].
+
+    The SK Hamiltonian over n spins is given as:
+        H(x)=-1/\sqrt{n} \sum_{i<j} w_{i,j}x_ix_j, where x_i\in\pm 1
+    and w_{i,j}\in\pm 1 are chosen independently and uniformly at random.
+    Notice that there are other variants e.g., with w_{i,j} chosen from the normal distribution
+    with mean 0 and variance 1.
+
+    References:
+        [1]: Dmitry Panchenko. "The Sherrington-Kirkpatrick model: an overview",
+        https://arxiv.org/abs/1211.1094
+    """
+
+    def __init__(self, n: int, rng: Optional[np.random.RandomState] = np.random):
+        """
+        Constructor for the SK model.
+
+        Args:
+            n: number of sites
+            rng: numpy pseudo-random number generator
+        """
+        self._rng = rng
+        self._n = n
+        self._graph = nx.complete_graph(self._n)
+
+        self.new_instance()
+
+    def new_instance(self) -> None:
+        """Generate a new instance of the SK model"""
+        for _, _, d in self._graph.edges(data=True):
+            d['weight'] = self._rng.choice([-1, 1])
+
+    def to_quadratic_program(self) -> QuadraticProgram:
+        """Convert an SK model problem instance into a
+        :class:`~qiskit_optimization.problems.QuadraticProgram`
+
+        Returns:
+            The :class:`~qiskit_optimization.problems.QuadraticProgram` created
+            from the SK problem instance.
+        """
+        mdl = Model(name="SK-model")
+        x = {
+            i: mdl.binary_var(name="x_{0}".format(i)) for i in range(self._graph.number_of_nodes())
+        }
+
+        objective = mdl.sum(
+            -1 / np.sqrt(self._n) * self._graph.edges[i, j]["weight"] * (2 * x[i] - 1) * (2 * x[j] - 1)
+            for i, j in self._graph.edges
+        )
+        # we converted the standard H(x)=-1/\sqrt{n} \sum w_{ij}x_ix_j, where x_i\in\pm 1 to binary.
+
+        mdl.minimize(objective)
+        op = from_docplex_mp(mdl)
+        return op
+
+    def interpret(self, result: Union[OptimizationResult, np.ndarray]) -> List[List[int]]:
+        """Interpret a result as energy and configuration of spins
+
+        Args:
+            result : The calculated result of the problem
+
+        Returns:
+            [energy, configuration of spins]
+        """
+        configuration = list(map(lambda x: 2 * x - 1, self._result_to_x(result)))
+        return [result.fval, configuration]
+
+    @property
+    def graph(self) -> nx.Graph:
+        """Getter of the graph
+        Returns:
+            A graph for a problem
+        """
+        return self._graph
+
+    def _set_rng(self, rng):
+        self._rng = rng
+
+    rng = property(fset=_set_rng)
---
+++ test/applications/test_SK_model.py
@@ -0,0 +1,81 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Test SK model class"""
+import numpy as np
+
+from test.optimization_test_case import QiskitOptimizationTestCase
+
+import networkx as nx
+
+from qiskit_optimization import QuadraticProgram
+from qiskit_optimization.algorithms import OptimizationResult, OptimizationResultStatus
+from qiskit_optimization.applications.SK_model import SKModel
+from qiskit_optimization.problems import QuadraticObjective, VarType
+
+
+class TestSKModel(QiskitOptimizationTestCase):
+    """Test SK model class"""
+
+    def setUp(self):
+        super().setUp()
+        self._n = 2
+        self._seed = 0
+        self._graph = nx.convert_matrix.from_numpy_matrix(np.array([[0., -1.], [-1., 0.]]))
+
+        op = QuadraticProgram()
+        for _ in range(2):
+            op.binary_var()
+
+        self._result = OptimizationResult(
+            x=[0, 0],
+            fval=-1 / np.sqrt(2),
+            variables=op.variables,
+            status=OptimizationResultStatus.SUCCESS,
+        )
+
+    def test_to_quadratic_program(self):
+        """Test to_quadratic_program"""
+        problem = SKModel(self._n, np.random.RandomState(self._seed))
+        qp = problem.to_quadratic_program()
+
+        # Test name
+        self.assertEqual(qp.name, "SK-model")
+
+        # Test variables
+        self.assertEqual(qp.get_num_vars(), 2)
+        for var in qp.variables:
+            self.assertEqual(var.vartype, VarType.BINARY)
+
+        # Test objective
+        obj = qp.objective
+        self.assertEqual(obj.sense, QuadraticObjective.Sense.MINIMIZE)
+        self.assertAlmostEqual(obj.constant, 1 / np.sqrt(2), 7)
+        obj_lin = obj.linear.to_dict()
+        self.assertAlmostEqual(obj_lin[0], -np.sqrt(2), 7)
+        self.assertAlmostEqual(obj_lin[1], -np.sqrt(2), 7)
+        obj_quad = obj.quadratic.to_dict()
+        self.assertEqual(len(obj_quad), 1)
+        self.assertAlmostEqual(obj_quad[(0, 1)], 2 * np.sqrt(2), 7)
+
+        # Test constraint
+        constraints_lin = qp.linear_constraints
+        self.assertEqual(len(constraints_lin), 0)
+        constraints_quad = qp.quadratic_constraints
+        self.assertEqual(len(constraints_quad), 0)
+
+    def test_interpret(self):
+        """Test interpret"""
+        sk = SKModel(2, np.random.RandomState(self._seed))
+        energy, configuration = sk.interpret(self._result)
+        self.assertEqual(configuration, [-1, -1])
+        self.assertAlmostEqual(energy, -1 / np.sqrt(2), 7)
