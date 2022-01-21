a329aede151370d87db805db452f54170d760ad4
Manoel Marques <manoel.marques@ibm.com>
2022-01-21 08:18:26 -0500

    Restrict gurobopy installation

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2021.
+# (C) Copyright IBM 2021, 2022.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -72,7 +72,7 @@ setuptools.setup(
         'cplex': ["cplex; python_version < '3.9'"],
         'cvx': ['cvxpy'],
         'matplotlib': ['matplotlib'],
-        'gurobi': ['gurobipy'],
+        'gurobi': ["gurobipy; python_version >= '3.7'"],
     },
     zip_safe=False
 )
