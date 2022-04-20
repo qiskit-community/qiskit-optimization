db32a23431a92a17aef156050fcc6c219851c78a
Takashi Imamichi <imamichi@jp.ibm.com>
2022-04-20 23:08:51 +0900

    add missing tests

---
+++ test/algorithms/test_optimization_result.py
@@ -0,0 +1,55 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+""" Test OptimizationResult """
+
+from test.optimization_test_case import QiskitOptimizationTestCase
+
+import numpy as np
+
+from qiskit_optimization.algorithms import OptimizationResult, OptimizationResultStatus
+from qiskit_optimization.problems import QuadraticProgram
+
+
+class TestOptimizationResult(QiskitOptimizationTestCase):
+    """Test OptimizationResult"""
+
+    def test_init(self):
+        """test init"""
+        q_p = QuadraticProgram()
+        q_p.integer_var_list(3)
+        result = OptimizationResult(
+            x=[1, 2, 3], fval=10, variables=q_p.variables, status=OptimizationResultStatus.SUCCESS
+        )
+        np.testing.assert_allclose(result.x, [1, 2, 3])
+        self.assertAlmostEqual(result.fval, 10)
+        self.assertEqual(result.status, OptimizationResultStatus.SUCCESS)
+        self.assertIsNone(result.raw_results)
+        self.assertEqual(len(result.samples), 1)
+        sample = result.samples[0]
+        np.testing.assert_allclose(sample.x, [1, 2, 3])
+        self.assertAlmostEqual(sample.fval, 10)
+        self.assertEqual(sample.status, OptimizationResultStatus.SUCCESS)
+        self.assertAlmostEqual(sample.probability, 1)
+
+    def test_str_repr(self):
+        """test str and repr"""
+        q_p = QuadraticProgram()
+        q_p.integer_var_list(3)
+        result = OptimizationResult(
+            x=[1, 2, 3], fval=10, variables=q_p.variables, status=OptimizationResultStatus.SUCCESS
+        )
+        expected = "\n".join(
+            ["optimal function value: 10", "optimal value: x0=1, x1=2, x2=3", "status: SUCCESS"]
+        )
+        self.assertEqual(str(result), expected)
+        self.assertEqual(repr(result), "<OptimizationResult: x=[1 2 3], fval=10, status=SUCCESS)>")
---
+++ test/translators/test_prettyprint.py
@@ -0,0 +1,199 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Test prettyprint"""
+
+from test.optimization_test_case import QiskitOptimizationTestCase
+
+from qiskit_optimization.problems import QuadraticProgram
+from qiskit_optimization.translators import prettyprint
+
+
+class TestPrettyprint(QiskitOptimizationTestCase):
+    """Test prettyprint"""
+
+    @staticmethod
+    def _convert(out: str):
+        print('"\\n".join([')
+        for line in out.split("\n"):
+            print(f'"{line}",')
+        print("])")
+
+    def test_prettyprint(self):
+        """test prettyprint"""
+
+        with self.subTest("minimize 1"):
+            q_p = QuadraticProgram("test")
+            q_p.binary_var(name="x")
+            q_p.binary_var(name="y")
+            q_p.minimize(linear={"x": 1}, quadratic={("x", "y"): 1})
+            out = prettyprint(q_p)
+            expected = "\n".join(
+                [
+                    "Problem name: test",
+                    "",
+                    "Minimize",
+                    "  x y + x",
+                    "",
+                    "Subject to",
+                    "  No constraints",
+                    "",
+                    "  Binary variables (2)",
+                    "    x y",
+                    "",
+                ]
+            )
+            self.assertEqual(out, expected)
+
+        with self.subTest("maximize 1"):
+            q_p = QuadraticProgram("test")
+            q_p.integer_var(lowerbound=5, upperbound=10, name="x")
+            q_p.continuous_var(lowerbound=-1, name="y")
+            q_p.maximize(linear={"x": -1, "y": 2}, constant=3)
+            q_p.quadratic_constraint({}, {("x", "y"): -1}, "<=", 4)
+            out = prettyprint(q_p)
+            expected = "\n".join(
+                [
+                    "Problem name: test",
+                    "",
+                    "Maximize",
+                    "  -x + 2 y + 3",
+                    "",
+                    "Subject to",
+                    "  Quadratic constraints (1)",
+                    "    -x y <= 4  'q0'",
+                    "",
+                    "  Integer variables (1)",
+                    "    5 <= x <= 10",
+                    "",
+                    "  Continuous variables (1)",
+                    "    -1 <= y",
+                    "",
+                ]
+            )
+            self.assertEqual(out, expected)
+
+        with self.subTest("minimize 2"):
+            q_p = QuadraticProgram("my problem")
+            q_p.binary_var("x")
+            q_p.integer_var(-1, 5, "y")
+            q_p.continuous_var(-1, 5, "z")
+            q_p.minimize(1, {"x": 1, "y": -1, "z": 10}, {("x", "x"): 0.5, ("y", "z"): -1})
+            q_p.linear_constraint({"x": 1, "y": 2}, "==", 1, "lin_eq")
+            q_p.linear_constraint({"x": 1, "y": 2}, "<=", 1, "lin_leq")
+            q_p.linear_constraint({"x": 1, "y": 2}, ">=", 1, "lin_geq")
+            q_p.quadratic_constraint(
+                {"x": 1, "y": 1},
+                {("x", "x"): 1, ("y", "z"): -1, ("z", "z"): 2},
+                "==",
+                1,
+                "quad_eq",
+            )
+            q_p.quadratic_constraint(
+                {"x": 1, "y": 1},
+                {("x", "x"): 1, ("y", "z"): -1, ("z", "z"): 2},
+                "<=",
+                1,
+                "quad_leq",
+            )
+            q_p.quadratic_constraint(
+                {"x": 1, "y": 1},
+                {("x", "x"): 1, ("y", "z"): -1, ("z", "z"): 2},
+                ">=",
+                1,
+                "quad_geq",
+            )
+            out = prettyprint(q_p)
+            expected = "\n".join(
+                [
+                    "Problem name: my problem",
+                    "",
+                    "Minimize",
+                    "  0.5 x^2 - y z + x - y + 10 z + 1",
+                    "",
+                    "Subject to",
+                    "  Linear constraints (3)",
+                    "    x + 2 y == 1  'lin_eq'",
+                    "    x + 2 y <= 1  'lin_leq'",
+                    "    x + 2 y >= 1  'lin_geq'",
+                    "",
+                    "  Quadratic constraints (3)",
+                    "    x^2 - y z + 2 z^2 + x + y == 1  'quad_eq'",
+                    "    x^2 - y z + 2 z^2 + x + y <= 1  'quad_leq'",
+                    "    x^2 - y z + 2 z^2 + x + y >= 1  'quad_geq'",
+                    "",
+                    "  Integer variables (1)",
+                    "    -1 <= y <= 5",
+                    "",
+                    "  Continuous variables (1)",
+                    "    -1 <= z <= 5",
+                    "",
+                    "  Binary variables (1)",
+                    "    x",
+                    "",
+                ]
+            )
+            self.assertEqual(out, expected)
+
+        with self.subTest("maximize 2"):
+            q_p = QuadraticProgram("problem 1")
+            q_p.integer_var(-1, 2, "x")
+            q_p.integer_var(-1, 2, "y")
+            q_p.continuous_var(-1, name="z")
+            q_p.binary_var("u")
+            q_p.binary_var("very_long_variable")
+            q_p.binary_var_list(10)
+            q_p.integer_var_list(3)
+            q_p.continuous_var_list(3)
+            q_p.minimize(constant=3, linear={"x": 2, "y": 3}, quadratic={("u", "x"): -1})
+            q_p.linear_constraint({"x": 1, "y": -2}, ">=", 2, name="lin_GE")
+            q_p.linear_constraint({"x": 2, "y": -1}, "==", 1, name="lin_EQ")
+            q_p.quadratic_constraint(
+                {"x": 1, "u": 1}, {(3, 4): 1, (5, 6): -1}, "<=", 1, name="quad_LE"
+            )
+            q_p.quadratic_constraint({"x": 2, "y": -1}, {("z", "z"): -1}, "<=", 1)
+            out = prettyprint(q_p)
+            expected = "\n".join(
+                [
+                    "Problem name: problem 1",
+                    "",
+                    "Minimize",
+                    "  -x u + 2 x + 3 y + 3",
+                    "",
+                    "Subject to",
+                    "  Linear constraints (2)",
+                    "    x - 2 y >= 2  'lin_GE'",
+                    "    2 x - y == 1  'lin_EQ'",
+                    "",
+                    "  Quadratic constraints (2)",
+                    "    u very_long_variable - x5 x6 + u + x <= 1  'quad_LE'",
+                    "    -z^2 + 2 x - y <= 1  'q1'",
+                    "",
+                    "  Integer variables (5)",
+                    "    -1 <= x <= 2",
+                    "    -1 <= y <= 2",
+                    "    0 <= x15",
+                    "    0 <= x16",
+                    "    0 <= x17",
+                    "",
+                    "  Continuous variables (4)",
+                    "    -1 <= z",
+                    "    0 <= x18",
+                    "    0 <= x19",
+                    "    0 <= x20",
+                    "",
+                    "  Binary variables (12)",
+                    "    u very_long_variable x5 x6 x7 x8 x9 x10 x11 x12 x13 x14",
+                    "",
+                ]
+            )
+            self.assertEqual(out, expected)
