1675ac5a5260e27d66ac1799854b00a159fef2e6
Takashi Imamichi <imamichi@jp.ibm.com>
2022-04-20 17:09:54 +0900

    add test for variable

--- test/problems/test_variable.py
+++ test/problems/test_variable.py
@@ -51,6 +51,50 @@ class TestVariable(QiskitOptimizationTestCase):
         self.assertEqual(variable.upperbound, INFINITY)
         self.assertEqual(variable.vartype, Variable.Type.CONTINUOUS)
 
+    def test_str_repr(self):
+        """test str and repr"""
+        quadratic_program = QuadraticProgram()
+
+        with self.subTest("binary"):
+            bin_var = quadratic_program.binary_var(name="x")
+            expected = "x (binary)"
+            self.assertEqual(str(bin_var), expected)
+            self.assertEqual(repr(bin_var), f"<Variable: {expected}>")
+
+        with self.subTest("integer 1"):
+            int_var = quadratic_program.integer_var(name="y1")
+            expected = "0 <= y1 (integer)"
+            self.assertEqual(str(int_var), expected)
+            self.assertEqual(repr(int_var), f"<Variable: {expected}>")
+
+        with self.subTest("integer 2"):
+            int_var2 = quadratic_program.integer_var(lowerbound=5, upperbound=10, name="y2")
+            expected = "5 <= y2 <= 10 (integer)"
+            self.assertEqual(str(int_var2), expected)
+            self.assertEqual(repr(int_var2), f"<Variable: {expected}>")
+
+        with self.subTest("continuous 1"):
+            con_var = quadratic_program.continuous_var(name="z1")
+            expected = "0 <= z1 (continuous)"
+            self.assertEqual(str(con_var), expected)
+            self.assertEqual(repr(con_var), f"<Variable: {expected}>")
+
+        with self.subTest("continuous 2"):
+            con_var2 = quadratic_program.continuous_var(
+                lowerbound=-10.0, upperbound=100.0, name="z2"
+            )
+            expected = "-10.0 <= z2 <= 100.0 (continuous)"
+            self.assertEqual(str(con_var2), expected)
+            self.assertEqual(repr(con_var2), f"<Variable: {expected}>")
+
+        with self.subTest("continuous 3"):
+            con_var3 = quadratic_program.continuous_var(
+                lowerbound=-INFINITY, upperbound=100.0, name="z3"
+            )
+            expected = "z3 <= 100.0 (continuous)"
+            self.assertEqual(str(con_var3), expected)
+            self.assertEqual(repr(con_var3), f"<Variable: {expected}>")
+
 
 if __name__ == "__main__":
     unittest.main()
