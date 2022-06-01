82ae9bae74547a79c6943872a701abdc55d4bee1
Manoel Marques <manoel.marques@ibm.com>
2022-06-01 15:24:03 -0400

    Pin pylint to < 2.14.0

--- test/problems/test_quadratic_expression.py
+++ test/problems/test_quadratic_expression.py
@@ -67,11 +67,11 @@ class TestQuadraticExpression(QiskitOptimizationTestCase):
                 coefficients[min(i, j)][max(i, j)] += i * j
         quadratic = QuadraticExpression(quadratic_program, coefficients)
         for i, j_v in enumerate(coefficients):
-            for j, _ in enumerate(j_v):
+            for j, j_v_j in enumerate(j_v):
                 if i == j:
-                    self.assertEqual(quadratic[i, j], coefficients[i][j])
+                    self.assertEqual(quadratic[i, j], j_v_j)
                 else:
-                    self.assertEqual(quadratic[i, j], coefficients[i][j] + coefficients[j][i])
+                    self.assertEqual(quadratic[i, j], j_v_j + coefficients[j][i])
 
     def test_setters(self):
         """test setters."""
