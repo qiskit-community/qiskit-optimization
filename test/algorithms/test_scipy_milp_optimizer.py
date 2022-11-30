8c0cd3a89875b7dfb33c1f8ff6738c423515f1b1
YZ <dran.z@foxmail.com>
2022-11-30 09:33:35 +0100

    Update

--- test/algorithms/test_scipy_milp_optimizer.py
+++ test/algorithms/test_scipy_milp_optimizer.py
@@ -18,13 +18,12 @@ from test.optimization_test_case import QiskitOptimizationTestCase
 from ddt import data, ddt
 import numpy as np
 
-import qiskit_optimization.optionals as _optionals
 from qiskit_optimization.problems import QuadraticProgram
 from qiskit_optimization.algorithms import ScipyMilpOptimizer
 
 
 @ddt
-class TestGurobiOptimizer(QiskitOptimizationTestCase):
+class TestScipyMilpOptimizer(QiskitOptimizationTestCase):
     """ScipyMilp Optimizer Tests."""
 
     @data(
@@ -32,8 +31,6 @@ class TestGurobiOptimizer(QiskitOptimizationTestCase):
         ("op_mip1.lp", [0, 1, 1], 5.5),
         ("op_lp1.lp", [0.25, 1.75], 5.8750),
     )
-    @unittest.skipIf(not _optionals.HAS_GUROBIPY, "Gurobi not available.")
-    @unittest.skipIf(not _optionals.HAS_CPLEX, "CPLEX not available.")
     def test_scipy_milp_optimizer(self, config):
         """ScipyMilp Optimizer Test"""
         # unpack configuration
@@ -45,7 +42,7 @@ class TestGurobiOptimizer(QiskitOptimizationTestCase):
         lp_file = self.get_resource_path(filename, "algorithms/resources")
         problem.read_from_lp_file(lp_file)
 
-        # solve problem with gurobi
+        # solve problem with ScipyMilpSolver
         result = scipy_milp_solver.solve(problem)
 
         # analyze results
