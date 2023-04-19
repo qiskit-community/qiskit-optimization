8d1c5708e3ec197e137b3fc844d87b707ef8f248
Takashi Imamichi <t.imamichi@gmail.com>
2023-04-19 15:47:01 +0900

    Depcate runtime client

---
+++ test/runtime/test_qaoaclient.py
@@ -0,0 +1,64 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Test the QAOA client."""
+
+import unittest
+import warnings
+from test import QiskitOptimizationTestCase
+
+import numpy as np
+from ddt import data, ddt
+from qiskit.algorithms.optimizers import COBYLA
+from qiskit.opflow import I, Z
+from qiskit.providers.basicaer import QasmSimulatorPy
+
+from qiskit_optimization.runtime import QAOAClient, VQERuntimeResult
+
+from .fake_vqeruntime import FakeQAOARuntimeProvider
+
+
+@ddt
+class TestQAOAClient(QiskitOptimizationTestCase):
+    """Test the QAOA client."""
+
+    def setUp(self):
+        super().setUp()
+        self.provider = FakeQAOARuntimeProvider()
+
+    @data(
+        {"name": "SPSA", "maxiter": 100},
+        COBYLA(),
+    )
+    def test_standard_case(self, optimizer):
+        """Test a standard use case."""
+        reps = 2
+        initial_point = np.random.RandomState(42).random(2 * reps)
+        backend = QasmSimulatorPy()
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=DeprecationWarning)
+            operator = Z ^ I ^ Z
+            qaoa = QAOAClient(
+                optimizer=optimizer,
+                reps=reps,
+                initial_point=initial_point,
+                backend=backend,
+                provider=self.provider,
+            )
+            result = qaoa.compute_minimum_eigenvalue(operator)
+
+        self.assertIsInstance(result, VQERuntimeResult)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/runtime/test_vqeclient.py
@@ -0,0 +1,65 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Test the VQE client."""
+
+import unittest
+import warnings
+from test import QiskitOptimizationTestCase
+
+import numpy as np
+from ddt import data, ddt
+from qiskit.algorithms.optimizers import COBYLA
+from qiskit.circuit.library import RealAmplitudes
+from qiskit.opflow import I, Z
+from qiskit.providers.basicaer import QasmSimulatorPy
+
+from qiskit_optimization.runtime import VQEClient, VQERuntimeResult
+
+from .fake_vqeruntime import FakeVQERuntimeProvider
+
+
+@ddt
+class TestVQEClient(QiskitOptimizationTestCase):
+    """Test the VQE client."""
+
+    def setUp(self):
+        super().setUp()
+        self.provider = FakeVQERuntimeProvider()
+
+    @data(
+        {"name": "SPSA", "maxiter": 100},
+        COBYLA(),
+    )
+    def test_standard_case(self, optimizer):
+        """Test a standard use case."""
+        circuit = RealAmplitudes(3)
+        initial_point = np.random.RandomState(42).random(circuit.num_parameters)
+        backend = QasmSimulatorPy()
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=DeprecationWarning)
+            operator = Z ^ I ^ Z
+            vqe = VQEClient(
+                ansatz=circuit,
+                optimizer=optimizer,
+                initial_point=initial_point,
+                backend=backend,
+                provider=self.provider,
+            )
+            result = vqe.compute_minimum_eigenvalue(operator)
+
+        self.assertIsInstance(result, VQERuntimeResult)
+
+
+if __name__ == "__main__":
+    unittest.main()
