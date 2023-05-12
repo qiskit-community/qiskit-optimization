e2a582b029d4d98002ba4ae69b142d6c166717d7
Eric Arellano <ericarellano@me.com>
2023-05-12 08:50:44 -0600

    Remove unnecessary CSS files

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,6 @@ extensions = [
 ]
 html_static_path = ["_static"]
 templates_path = ["_templates"]
-html_css_files = ["style.css", "custom.css", "gallery.css"]
 
 nbsphinx_timeout = 180
 nbsphinx_execute = os.getenv("QISKIT_DOCS_BUILD_TUTORIALS", "never")
