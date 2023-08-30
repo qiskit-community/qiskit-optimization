b3bbd535f5ff71e84b9af7ef2254fa45a1f2ec21
Takashi Imamichi <imamichi@jp.ibm.com>
2023-08-30 17:00:34 +0900

    remove runtime program and quantum instance

--- qiskit_optimization/algorithms/warm_start_qaoa_optimizer.py
+++ qiskit_optimization/algorithms/warm_start_qaoa_optimizer.py
@@ -18,7 +18,6 @@ from typing import Dict, List, Optional, Tuple, Union, cast
 
 import numpy as np
 from qiskit import QuantumCircuit
-from qiskit.algorithms import QAOA as LegacyQAOA
 from qiskit.algorithms.minimum_eigensolvers import QAOA
 from qiskit.circuit import Parameter
 
@@ -203,7 +202,7 @@ class WarmStartQAOAOptimizer(MinimumEigenOptimizer):
         self,
         pre_solver: OptimizationAlgorithm,
         relax_for_pre_solver: bool,
-        qaoa: Union[QAOA, LegacyQAOA],
+        qaoa: QAOA,
         epsilon: float = 0.25,
         num_initial_solutions: int = 1,
         warm_start_factory: Optional[WarmStartQAOAFactory] = None,
