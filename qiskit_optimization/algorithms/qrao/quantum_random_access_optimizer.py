3b4570e529aaee24cb751e4046e09cb98a0dfafd
a-matsuo <MATSUOA@jp.ibm.com>
2023-09-07 21:57:49 +0900

    fix mypy

--- qiskit_optimization/algorithms/qrao/quantum_random_access_optimizer.py
+++ qiskit_optimization/algorithms/qrao/quantum_random_access_optimizer.py
@@ -224,9 +224,7 @@ class QuantumRandomAccessOptimizer(OptimizationAlgorithm):
         # Get auxiliary expectation values for rounding.
         expectation_values: list[complex] | None = None
         if relaxed_result.aux_operators_evaluated is not None:
-            expectation_values = [
-                v[0] for v in relaxed_result.aux_operators_evaluated  # type: ignore
-            ]
+            expectation_values = [v[0] for v in relaxed_result.aux_operators_evaluated]
 
         # Get the circuit corresponding to the relaxed solution.
         if isinstance(relaxed_result, VariationalResult):
