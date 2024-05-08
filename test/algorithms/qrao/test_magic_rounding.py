4508f4aa7cd6ce7f90aed0a12f4c0b639f3f01a6
Takashi Imamichi <imamichi@jp.ibm.com>
2024-05-08 12:58:05 +0900

    bump black version

--- test/algorithms/qrao/test_magic_rounding.py
+++ test/algorithms/qrao/test_magic_rounding.py
@@ -1,6 +1,6 @@
 # This code is part of a Qiskit project.
 #
-# (C) Copyright IBM 2023.
+# (C) Copyright IBM 2023, 2024.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -328,9 +328,11 @@ def make_solution_sample(
         x=x,
         fval=problem.objective.evaluate(x),
         probability=probability,
-        status=OptimizationResultStatus.SUCCESS
-        if problem.is_feasible(x)
-        else OptimizationResultStatus.INFEASIBLE,
+        status=(
+            OptimizationResultStatus.SUCCESS
+            if problem.is_feasible(x)
+            else OptimizationResultStatus.INFEASIBLE
+        ),
     )
 
 
