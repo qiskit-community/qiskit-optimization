5e08503d3514f7d964991d78d9a2ce6004d24699
Apostolos Chalkis <tolis.chal@gmail.com>
2024-08-23 14:31:53 +0300

    Add utils from qiskit-algorithms for qaoa compatibility

---
+++ qiskit_optimization/utils/__init__.py
@@ -0,0 +1,23 @@
+# This code is part of a Qiskit project.
+#
+# (C) Copyright IBM 2022, 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Common qiskit_algorithms utility functions."""
+
+from .algorithm_globals import algorithm_globals
+from .validate_initial_point import validate_initial_point
+from .validate_bounds import validate_bounds
+
+__all__ = [
+    "algorithm_globals",
+    "validate_initial_point",
+    "validate_bounds",
+]
