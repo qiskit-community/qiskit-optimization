7c879552ec0c261e81b96c41e9f333ef63af5bfc
Apostolos Chalkis <tolis.chal@gmail.com>
2024-09-03 16:06:53 +0300

    add tests from qiskit-qlgorithms for the newly added code

---
+++ test/optimizers/test_optimizers.py
@@ -0,0 +1,251 @@
+# This code is part of a Qiskit project.
+#
+# (C) Copyright IBM 2018, 2024.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Test Optimizers"""
+
+import unittest
+from test import QiskitAlgorithmsTestCase
+
+from typing import Optional, List, Tuple
+from ddt import ddt, data, unpack
+import numpy as np
+from scipy.optimize import rosen, rosen_der
+
+from qiskit.exceptions import MissingOptionalLibraryError
+
+from qiskit_optimization.optimizers import (
+    COBYLA,
+    NELDER_MEAD,
+    Optimizer,
+    SPSA,
+    SciPyOptimizer,
+)
+from qiskit_optimization.utils import algorithm_globals
+
+
+@ddt
+class TestOptimizers(QiskitAlgorithmsTestCase):
+    """Test Optimizers"""
+
+    def setUp(self):
+        super().setUp()
+        algorithm_globals.random_seed = 52
+
+    def run_optimizer(
+        self,
+        optimizer: Optimizer,
+        max_nfev: int,
+        grad: bool = False,
+        bounds: Optional[List[Tuple[float, float]]] = None,
+    ):
+        """Test the optimizer.
+
+        Args:
+            optimizer: The optimizer instance to test.
+            max_nfev: The maximal allowed number of function evaluations.
+            grad: Whether to pass the gradient function as input.
+            bounds: Optimizer bounds.
+        """
+        x_0 = np.asarray([1.3, 0.7, 0.8, 1.9, 1.2])
+        jac = rosen_der if grad else None
+
+        res = optimizer.minimize(rosen, x_0, jac, bounds)
+        x_opt = res.x
+        nfev = res.nfev
+
+        np.testing.assert_array_almost_equal(x_opt, [1.0] * len(x_0), decimal=2)
+        self.assertLessEqual(nfev, max_nfev)
+
+    def test_cobyla(self):
+        """cobyla test"""
+        optimizer = COBYLA(maxiter=100000, tol=1e-06)
+        self.run_optimizer(optimizer, max_nfev=100000)
+
+    def test_nelder_mead(self):
+        """nelder mead test"""
+        optimizer = NELDER_MEAD(maxfev=10000, tol=1e-06)
+        self.run_optimizer(optimizer, max_nfev=10000)
+
+    @unittest.skip("Skipping SPSA as it does not do well on non-convex rozen")
+    def test_spsa(self):
+        """spsa test"""
+        optimizer = SPSA(maxiter=10000)
+        self.run_optimizer(optimizer, max_nfev=100000)
+
+    def test_scipy_optimizer(self):
+        """scipy_optimizer test"""
+        optimizer = SciPyOptimizer("BFGS", options={"maxiter": 1000})
+        self.run_optimizer(optimizer, max_nfev=10000)
+
+    def test_scipy_optimizer_callback(self):
+        """scipy_optimizer callback test"""
+        values = []
+
+        def callback(x):
+            values.append(x)
+
+        optimizer = SciPyOptimizer("BFGS", options={"maxiter": 1000}, callback=callback)
+        self.run_optimizer(optimizer, max_nfev=10000)
+        self.assertTrue(values)  # Check the list is nonempty.
+
+    def test_scipy_optimizer_parse_bounds(self):
+        """
+        Test the parsing of bounds in SciPyOptimizer.minimize method. Verifies that the bounds are
+        correctly parsed and set within the optimizer object.
+
+        Raises:
+            AssertionError: If any of the assertions fail.
+            AssertionError: If a TypeError is raised unexpectedly while parsing bounds.
+
+        """
+        try:
+            # Initialize SciPyOptimizer instance with SLSQP method
+            optimizer = SciPyOptimizer("SLSQP")
+
+            # Call minimize method with a simple lambda function and bounds
+            optimizer.minimize(lambda x: -x, 1.0, bounds=[(0.0, 1.0)])
+
+            # Assert that "bounds" is not present in optimizer options and kwargs
+            self.assertFalse("bounds" in optimizer._options)
+            self.assertFalse("bounds" in optimizer._kwargs)
+
+        except TypeError:
+            # This would give: https://github.com/qiskit-community/qiskit-machine-learning/issues/570
+            self.fail(
+                "TypeError was raised unexpectedly when parsing bounds in SciPyOptimizer.minimize(...)."
+            )
+
+        # Finally, expect exceptions if bounds are parsed incorrectly, i.e. differently than as in Scipy
+        with self.assertRaises(RuntimeError):
+            _ = SciPyOptimizer("SLSQP", bounds=[(0.0, 1.0)])
+        with self.assertRaises(RuntimeError):
+            _ = SciPyOptimizer("SLSQP", options={"bounds": [(0.0, 1.0)]})
+
+    @unpack
+    def test_nlopt(self, optimizer_cls, use_bound):
+        """NLopt test"""
+        bounds = [(-6, 6)] * 5 if use_bound else None
+        try:
+            optimizer = optimizer_cls()
+            optimizer.set_options(**{"max_evals": 50000})
+            self.run_optimizer(optimizer, max_nfev=50000, bounds=bounds)
+        except MissingOptionalLibraryError as ex:
+            self.skipTest(str(ex))
+
+
+@ddt
+class TestOptimizerSerialization(QiskitAlgorithmsTestCase):
+    """Tests concerning the serialization of optimizers."""
+
+    @data(
+        ("COBYLA", {"maxiter": 10}),
+        ("NELDER_MEAD", {"maxiter": 0}),
+        ("dogleg", {"maxiter": 100}),
+        ("trust-constr", {"maxiter": 10}),
+        ("trust-ncg", {"maxiter": 100}),
+        ("trust-exact", {"maxiter": 120}),
+        ("trust-krylov", {"maxiter": 150}),
+    )
+    @unpack
+    def test_scipy(self, method, options):
+        """Test the SciPyOptimizer is serializable."""
+
+        optimizer = SciPyOptimizer(method, options=options)
+        serialized = optimizer.settings
+        from_dict = SciPyOptimizer(**serialized)
+
+        self.assertEqual(from_dict._method, method.lower())
+        self.assertEqual(from_dict._options, options)
+
+    def test_independent_reconstruction(self):
+        """Test the SciPyOptimizers don't reset all settings upon creating a new instance.
+
+        COBYLA is used as representative example here."""
+
+        kwargs = {"coffee": "without sugar"}
+        options = {"tea": "with milk"}
+        optimizer = COBYLA(maxiter=1, options=options, **kwargs)
+        serialized = optimizer.settings
+        from_dict = COBYLA(**serialized)
+
+        with self.subTest(msg="test attributes"):
+            self.assertEqual(from_dict.settings["maxiter"], 1)
+
+        with self.subTest(msg="test options"):
+            # options should only contain values that are *not* already in the initializer
+            # (e.g. should not contain maxiter)
+            self.assertEqual(from_dict.settings["options"], {"tea": "with milk"})
+
+        with self.subTest(msg="test kwargs"):
+            self.assertEqual(from_dict.settings["coffee"], "without sugar")
+
+        with self.subTest(msg="option ids differ"):
+            self.assertNotEqual(id(serialized["options"]), id(from_dict.settings["options"]))
+
+    def test_spsa(self):
+        """Test SPSA optimizer is serializable."""
+        options = {
+            "maxiter": 100,
+            "blocking": True,
+            "allowed_increase": 0.1,
+            "second_order": True,
+            "learning_rate": 0.02,
+            "perturbation": 0.05,
+            "regularization": 0.1,
+            "resamplings": 2,
+            "perturbation_dims": 5,
+            "trust_region": False,
+            "initial_hessian": None,
+            "lse_solver": None,
+            "hessian_delay": 0,
+            "callback": None,
+            "termination_checker": None,
+        }
+        spsa = SPSA(**options)
+
+        self.assertDictEqual(spsa.settings, options)
+
+    def test_spsa_custom_iterators(self):
+        """Test serialization works with custom iterators for learning rate and perturbation."""
+        rate = 0.99
+
+        def powerlaw():
+            n = 0
+            while True:
+                yield rate**n
+                n += 1
+
+        def steps():
+            n = 1
+            divide_after = 20
+            epsilon = 0.5
+            while True:
+                yield epsilon
+                n += 1
+                if n % divide_after == 0:
+                    epsilon /= 2
+
+        learning_rate = powerlaw()
+        expected_learning_rate = np.array([next(learning_rate) for _ in range(200)])
+
+        perturbation = steps()
+        expected_perturbation = np.array([next(perturbation) for _ in range(200)])
+
+        spsa = SPSA(maxiter=200, learning_rate=powerlaw, perturbation=steps)
+        settings = spsa.settings
+
+        self.assertTrue(np.allclose(settings["learning_rate"], expected_learning_rate))
+        self.assertTrue(np.allclose(settings["perturbation"], expected_perturbation))
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/optimizers/test_spsa.py
@@ -0,0 +1,182 @@
+# This code is part of a Qiskit project.
+#
+# (C) Copyright IBM 2021, 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Tests for the SPSA optimizer."""
+
+from test import QiskitAlgorithmsTestCase
+from ddt import ddt, data
+
+import numpy as np
+
+from qiskit.circuit.library import PauliTwoDesign
+from qiskit.quantum_info import SparsePauliOp, Statevector
+
+from qiskit_optimization.optimizers import SPSA
+from qiskit_optimization.utils import algorithm_globals
+
+
+@ddt
+class TestSPSA(QiskitAlgorithmsTestCase):
+    """Tests for the SPSA optimizer."""
+
+    def setUp(self):
+        super().setUp()
+        np.random.seed(12)
+        algorithm_globals.random_seed = 12
+
+    # @slow_test
+    def test_pauli_two_design(self):
+        """Test SPSA on the Pauli two-design example."""
+        circuit = PauliTwoDesign(3, reps=1, seed=1)
+        parameters = list(circuit.parameters)
+        obs = SparsePauliOp("ZZI")  # Z^Z^I
+
+        initial_point = np.array(
+            [0.82311034, 0.02611798, 0.21077064, 0.61842177, 0.09828447, 0.62013131]
+        )
+
+        def objective(x):
+            bound_circ = circuit.assign_parameters(dict(zip(parameters, x)))
+            return Statevector(bound_circ).expectation_value(obs).real
+
+        settings = {"maxiter": 100, "blocking": True, "allowed_increase": 0}
+
+
+        expected_nfev = settings["maxiter"] * 3 + 1
+
+        spsa = SPSA(**settings)
+
+        result = spsa.minimize(objective, x0=initial_point)
+
+        with self.subTest("check final accuracy"):
+            self.assertLess(result.fun, -0.95)  # final loss
+
+        with self.subTest("check number of function calls"):
+            self.assertEqual(result.nfev, expected_nfev)  # function evaluations
+
+    def test_recalibrate_at_optimize(self):
+        """Test SPSA calibrates anew upon each optimization run, if no auto-calibration is set."""
+
+        def objective(x):
+            return -(x**2)
+
+        spsa = SPSA(maxiter=1)
+        _ = spsa.minimize(objective, x0=np.array([0.5]))
+
+        self.assertIsNone(spsa.learning_rate)
+        self.assertIsNone(spsa.perturbation)
+
+    def test_learning_rate_perturbation_as_iterators(self):
+        """Test the learning rate and perturbation can be callables returning iterators."""
+
+        def get_learning_rate():
+            def learning_rate():
+                x = 0.99
+                while True:
+                    x *= x
+                    yield x
+
+            return learning_rate
+
+        def get_perturbation():
+            def perturbation():
+                x = 0.99
+                while True:
+                    x *= x
+                    yield max(x, 0.01)
+
+            return perturbation
+
+        def objective(x):
+            return (np.linalg.norm(x) - 2) ** 2
+
+        spsa = SPSA(learning_rate=get_learning_rate(), perturbation=get_perturbation())
+        result = spsa.minimize(objective, np.array([0.5, 0.5]))
+
+        self.assertAlmostEqual(np.linalg.norm(result.x), 2, places=2)
+
+    def test_learning_rate_perturbation_as_arrays(self):
+        """Test the learning rate and perturbation can be arrays."""
+
+        learning_rate = np.linspace(1, 0, num=100, endpoint=False) ** 2
+        perturbation = 0.01 * np.ones(100)
+
+        def objective(x):
+            return (np.linalg.norm(x) - 2) ** 2
+
+        spsa = SPSA(learning_rate=learning_rate, perturbation=perturbation)
+        result = spsa.minimize(objective, x0=np.array([0.5, 0.5]))
+
+        self.assertAlmostEqual(np.linalg.norm(result.x), 2, places=2)
+
+    def test_termination_checker(self):
+        """Test the termination_callback"""
+
+        def objective(x):
+            return np.linalg.norm(x) + np.random.rand(1)
+
+        class TerminationChecker:
+            """Example termination checker"""
+
+            def __init__(self):
+                self.values = []
+
+            def __call__(self, nfev, point, fvalue, stepsize, accepted) -> bool:
+                self.values.append(fvalue)
+
+                if len(self.values) > 10:
+                    return True
+                return False
+
+        maxiter = 400
+        spsa = SPSA(maxiter=maxiter, termination_checker=TerminationChecker())
+        result = spsa.minimize(objective, x0=[0.5, 0.5])
+
+        self.assertLess(result.nit, maxiter)
+
+    def test_callback(self):
+        """Test using the callback."""
+
+        def objective(x):
+            return (np.linalg.norm(x) - 2) ** 2
+
+        history = {"nfevs": [], "points": [], "fvals": [], "updates": [], "accepted": []}
+
+        def callback(nfev, point, fval, update, accepted):
+            history["nfevs"].append(nfev)
+            history["points"].append(point)
+            history["fvals"].append(fval)
+            history["updates"].append(update)
+            history["accepted"].append(accepted)
+
+        maxiter = 10
+        spsa = SPSA(maxiter=maxiter, learning_rate=0.01, perturbation=0.01, callback=callback)
+        _ = spsa.minimize(objective, x0=np.array([0.5, 0.5]))
+
+        expected_types = [int, np.ndarray, float, float, bool]
+        for i, (key, values) in enumerate(history.items()):
+            self.assertTrue(all(isinstance(value, expected_types[i]) for value in values))
+            self.assertEqual(len(history[key]), maxiter)
+
+    @data(1, 2, 3, 4)
+    def test_estimate_stddev(self, max_evals_grouped):
+        """Test the estimate_stddev
+        See https://github.com/Qiskit/qiskit-nature/issues/797"""
+
+        def objective(x):
+            if len(x.shape) == 2:
+                return np.array([sum(x_i) for x_i in x])
+            return sum(x)
+
+        point = np.ones(5)
+        result = SPSA.estimate_stddev(objective, point, avg=10, max_evals_grouped=max_evals_grouped)
+        self.assertAlmostEqual(result, 0)
