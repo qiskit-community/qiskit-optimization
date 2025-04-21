db62fd8b06123704558e94171aeaac1b1cc5958a
Takashi Imamichi <imamichi@jp.ibm.com>
2025-04-21 16:25:36 +0900

    enable python 3.13

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Scientific/Engineering",
     ],
     keywords="qiskit sdk quantum optimization",
